77a58264b6c47e2dc4d20d7dc10dd270da03a862
MurilloMorgado <mumorgado@hotmail.com>
2025-07-08 18:30:06 -0300

    first commit

---
+++ src/main/java/murillo/com/br/fast_double_click/FastDoubleClickApplication.java
@@ -0,0 +1,13 @@
+package murillo.com.br.fast_double_click;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastDoubleClickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastDoubleClickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/murillo/com/br/fast_double_click/FastDoubleClickApplicationTests.java
@@ -0,0 +1,13 @@
+package murillo.com.br.fast_double_click;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastDoubleClickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
