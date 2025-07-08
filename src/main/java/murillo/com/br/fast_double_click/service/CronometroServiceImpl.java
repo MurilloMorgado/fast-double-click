2c5148f9d4962040d8672571e2867e5283f9a9ea
MurilloMorgado <mumorgado@hotmail.com>
2025-07-08 18:47:18 -0300

    Criado o esqueleto da implementacao do cronometro, aonde vai obter as regras de negocio

---
+++ src/main/java/murillo/com/br/fast_double_click/service/CronometroServiceImpl.java
@@ -0,0 +1,24 @@
+package murillo.com.br.fast_double_click.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import murillo.com.br.fast_double_click.model.RegistroTempo;
+
+@Service
+public class CronometroServiceImpl implements CronometroService {
+
+  @Override
+  public List<RegistroTempo> listarTempos() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'listarTempos'");
+  }
+
+  @Override
+  public RegistroTempo registrarTempo(RegistroTempo registroTempo) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'registrarTempo'");
+  }
+  
+}
