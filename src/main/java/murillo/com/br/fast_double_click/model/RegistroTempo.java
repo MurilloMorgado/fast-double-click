1b4e4e8ac2bbb8b16d08b483f407b4f470f14bdf
MurilloMorgado <mumorgado@hotmail.com>
2025-07-08 18:33:17 -0300

    model registroTempo

---
+++ src/main/java/murillo/com/br/fast_double_click/model/RegistroTempo.java
@@ -0,0 +1,12 @@
+package murillo.com.br.fast_double_click.model;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class RegistroTempo {
+  
+  String tempo;
+  LocalDateTime data;
+}
