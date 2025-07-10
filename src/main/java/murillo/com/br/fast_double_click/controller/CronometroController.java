3369f05c7b035e81109d14eb3b30ccf0d7cbc962
MurilloMorgado <mumorgado@hotmail.com>
2025-07-10 10:28:50 -0300

    implementando o crossOrigin

--- src/main/java/murillo/com/br/fast_double_click/controller/CronometroController.java
+++ src/main/java/murillo/com/br/fast_double_click/controller/CronometroController.java
@@ -3,6 +3,7 @@ package murillo.com.br.fast_double_click.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import murillo.com.br.fast_double_click.model.RegistroTempo;
 import murillo.com.br.fast_double_click.service.CronometroService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/cronometro")
 @RequiredArgsConstructor
 public class CronometroController {
